317e3985a6332baa2fe00cfe56f4f7879cf6f44d
KorolevAV <antoshka.992@mail.ru>
2022-10-26 17:58:39 +0300

    add check element equal zero

--- Seminar2/homework_001.java
+++ Seminar2/homework_001.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class homework_001 {
     public static void main(String[] args) {
-        String[][] array = { { "0", "1", "2", "4", "5" }, { "a", "b", "c", "d", "a" }, { "e", "f", "g", "h", "45" } };
+        String[][] array = { { "3", "1", "2", "4", "5" }, { "a", "b", "c", "d", "a" }, { "e", "f", "g", "h", "45" } };
         System.out.println(Arrays.deepToString(array));
         int sum = sum2d(array);
         System.out.println(sum);
@@ -14,6 +14,9 @@ public class homework_001 {
         int s = 0;
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < 5; j++) {
+                if (a[i][j] == "0") {
+                    throw new RuntimeException("В исходном массиве битые значения!");
+                }
                 try {
                     int val = Integer.parseInt(a[i][j]);
                     s += val;
